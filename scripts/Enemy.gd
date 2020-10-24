df00371acbd61da6b105d2231a1e137b38818f05
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-10-24 00:41:47 -0700

    Add blood effect to enemy deaths

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -6,8 +6,10 @@ var velocity = Vector2()
 
 var players
 
+var is_alive = true
 
-func _enter_tree():
+
+func _ready():
 	players = get_tree().get_nodes_in_group("players")
 
 
@@ -21,15 +23,19 @@ func _rotate_enemy():
 
 
 func _physics_process(delta):
-	_find_player()
-	_rotate_enemy()
-	
-	var collision_info = move_and_slide(velocity)
-	
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if collision.collider.name == "Blade":
-			queue_free()
-		if collision.collider.name == "Player":
-			players[0].queue_free()
+	if is_alive:
+		_find_player()
+		_rotate_enemy()
+		
+		var collision_info = move_and_slide(velocity)
+		
+		for i in get_slide_count():
+			var collision = get_slide_collision(i)
+			if collision.collider.name == "Blade":
+				is_alive = false
+				$CollisionShape2D.set_disabled(true)
+				$Sprite.visible = false
+				$Particles2D.set_emitting(true)
+			if collision.collider.name == "Player":
+				players[0].queue_free()
 
