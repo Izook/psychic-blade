82b6e226cd93a3dc346687b84ffd6786108d4c6f
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-10-23 22:35:26 -0700

    Have enemy face player

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -6,13 +6,21 @@ var velocity = Vector2()
 
 var players
 
+
 func _enter_tree():
 	players = get_tree().get_nodes_in_group("players")
 
-func find_player():
+
+func _find_player():
 	var player_position = players[0].position
 	velocity = position.direction_to(player_position)  * speed
 
+
+func _rotate_enemy():
+	set_global_rotation(velocity.angle() + PI / 2)
+
+
 func _physics_process(delta):
-	find_player()
+	_find_player()
+	_rotate_enemy()
 	move_and_slide(velocity)
