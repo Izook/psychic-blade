b8a6a8d2e00882b12c3f942ced0230ec427df462
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-10-23 21:31:25 -0700

    Blade angles itself appropriately around player while moving

--- scripts/Blade.gd
+++ scripts/Blade.gd
@@ -4,12 +4,15 @@ const MAX_RADIUS = 300
 const MIN_RADIUS = 100
 
 var polar_vector
+var blade_angle
 
 var radius = 200
 var radial_speed = 10
 
-var angle = 0
+var anglular_pos = 0
 var angular_speed = PI/48
+
+var blade_moved = false
 
 
 func _limit_radius(r):
@@ -22,9 +25,9 @@ func _limit_radius(r):
 
 func _get_input():
 	if Input.is_action_pressed('rotate_left'):
-		angle -= angular_speed
+		anglular_pos -= angular_speed
 	if Input.is_action_pressed('rotate_right'):
-		angle += angular_speed
+		anglular_pos += angular_speed
 	if Input.is_action_pressed('push_out'):
 		radius += radial_speed
 	if Input.is_action_pressed('pull_in'):
@@ -33,8 +36,19 @@ func _get_input():
 	radius = _limit_radius(radius)
 
 
-func _physics_process(delta):
-	_get_input()
-	polar_vector = polar2cartesian(radius, angle)
+func _move_blade():
+	polar_vector = polar2cartesian(radius, anglular_pos)
+	
+	if $Blade.position != polar_vector:
+		blade_angle = $Blade.position.angle_to_point(polar_vector)
+		$Blade.set_global_rotation(blade_angle + PI)
+	else:
+		$Blade.set_global_rotation(anglular_pos)
+		
 	$Blade.set_position(polar_vector)
+
+
+func _physics_process(_delta):
+	_get_input()
+	_move_blade()
 
