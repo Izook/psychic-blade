d200c7ea668df06f43dce82dbd4a502afb0815b8
Isaac Ortega <isaac.io.ortega@gmail.com>
2020-11-11 16:28:11 -0800

    Add acceleration and decceleration to blades angular position

--- scripts/Blade.gd
+++ scripts/Blade.gd
@@ -2,13 +2,19 @@ extends Node2D
 
 const MAX_RADIUS := 300
 const MIN_RADIUS := 150
+
+const MAX_ANGULAR_SPEED := PI/12
+
 const BLADE_SPEED_FACTOR := 20 
 
 export (int) var radius := 200
 export (int) var radial_speed := 10
 
 export (float) var angular_pos := 0.0
-export (float) var angular_speed := PI/36
+export (float) var angular_speed_index := 0.0
+export (float) var angular_speed_index_speed := 0.001
+export (float) var angular_speed_index_turning_speed := 0.008
+export (float) var angular_speed_index_damp := 0.001
 
 onready var blade_target := $BladeTarget as Sprite
 onready var blade_node := $Blade as KinematicBody2D
@@ -18,13 +24,31 @@ var blade_angle := 0.0
 
 func _get_input() -> void:
 	if Input.is_action_pressed('rotate_left'):
-		angular_pos -= angular_speed
-	if Input.is_action_pressed('rotate_right'):
-		angular_pos += angular_speed
+		if angular_speed_index > 0:
+			angular_speed_index += angular_speed_index_speed
+		else:
+			angular_speed_index += angular_speed_index_turning_speed
+	elif Input.is_action_pressed('rotate_right'):
+		if angular_speed_index < 0:
+			angular_speed_index -= angular_speed_index_speed
+		else:
+			angular_speed_index -= angular_speed_index_turning_speed
+	else:
+		if angular_speed_index > 0:
+			angular_speed_index -= angular_speed_index_damp
+		else:
+			angular_speed_index += angular_speed_index_damp
+	
 	if Input.is_action_pressed('push_out'):
 		radius += radial_speed
 	if Input.is_action_pressed('pull_in'):
 		radius -= radial_speed
+	
+	angular_speed_index = _limit_speed_index(angular_speed_index)
+	var angular_speed := _get_angular_speed(abs(angular_speed_index))
+	if angular_speed_index < 0.0:
+		angular_speed *= -1
+	angular_pos += angular_speed
 	
 	radius = _limit_radius(radius)
 
@@ -44,11 +68,17 @@ func _draw() -> void:
 
 
 func _limit_radius(r: int) -> int:
-	if r > MAX_RADIUS:
-		r = MAX_RADIUS
-	if r < MIN_RADIUS:
-		r = MIN_RADIUS
-	return r
+	return max(min(MAX_RADIUS, r), MIN_RADIUS) as int
+
+
+func _limit_speed_index(i : float) -> float:
+	return max(min(1, i), -1)
+
+
+# Returns the angular speed of the blade based on a position on the easeOutQuart
+# curve. Curve gotten from https://easings.net/#easeOutQuart
+func _get_angular_speed(i: float) -> float:
+	return (1 - pow(1 - i, 4)) * MAX_ANGULAR_SPEED
 
 
 func _move_blade() -> void:
