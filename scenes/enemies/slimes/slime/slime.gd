2c8534433a658a0923c85b1b1a42055b430e49fc
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-01-15 20:20:32 -0800

    Refactor slime function names as private

--- scenes/enemies/slimes/slime/slime.gd
+++ scenes/enemies/slimes/slime/slime.gd
@@ -23,31 +23,31 @@ func _physics_process(delta: float) -> void:
 	
 	match slime_state:
 		SlimeState.WANDERING:
-			wander(delta)
+			_wander(delta)
 		SlimeState.ATTACKING:
-			attack(delta)
+			_attack(delta)
 		SlimeState.DEAD:
 			pass
 
 
-func wander(delta: float) -> void:
-	var velocity := wander_direction * get_slime_speed(delta)
-	move_slime(velocity, delta)
+func _wander(delta: float) -> void:
+	var velocity := wander_direction * _get_slime_speed(delta)
+	_move_slime(velocity, delta)
 
 
-func attack(delta: float) -> void:
+func _attack(delta: float) -> void:
 	var player_direction = global_position.direction_to(player.global_position)
-	var velocity := (player_direction * get_slime_speed(delta)) as Vector2
-	move_slime(velocity, delta)
+	var velocity := (player_direction * _get_slime_speed(delta)) as Vector2
+	_move_slime(velocity, delta)
 
 
-func die() -> void:
+func _die() -> void:
 	slime_state = SlimeState.DEAD
 	hitbox.set_disabled(true)
 	animation_player.play("death")
 
 
-func move_slime(velocity: Vector2, delta: float) -> void:
+func _move_slime(velocity: Vector2, delta: float) -> void:
 	var collision_info := move_and_collide(velocity * delta)
 	if collision_info:
 		var blade := collision_info.collider as BladeHitbox
@@ -55,13 +55,13 @@ func move_slime(velocity: Vector2, delta: float) -> void:
 			die()
 
 
-func get_slime_speed(delta: float) -> float:
+func _get_slime_speed(delta: float) -> float:
 	speed_time += delta
 	speed_time = fmod(speed_time, IDLE_ANIMATION_FINISH)
 	return sin((2 * PI / 3) * speed_time) * MAX_SPEED
 
 
-func renew_wander_direction() -> void:
+func _renew_wander_direction() -> void:
 	wander_direction = Vector2(rand_range(-1, 1), rand_range(-1, 1))
 	raycast.set_cast_to(wander_direction * CAST_LENGTH)
 	
@@ -73,7 +73,7 @@ func renew_wander_direction() -> void:
 
 func _on_WanderingTimer_timeout() -> void:
 	if slime_state == SlimeState.WANDERING:
-		renew_wander_direction()
+		_renew_wander_direction()
 
 
 func _on_DetectionArea_body_entered(body: PhysicsBody2D) -> void:
