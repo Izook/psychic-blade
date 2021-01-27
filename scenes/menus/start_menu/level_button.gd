0b0aca1d416ade28362d5f80719bb5678b8aebea
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-01-26 22:36:49 -0800

    Make start menu accessible via controller

--- scenes/menus/start_menu/level_button.gd
+++ scenes/menus/start_menu/level_button.gd
@@ -1,17 +1,14 @@
-extends Label
+extends Button
 
 class_name LevelButton
 
-signal clicked(level_name)
+signal level_pressed(level_name)
 
 
 func _ready() -> void:
-	var _self_conn_error = connect("gui_input", self, "_on_self_gui_input")
-	var _menu_conn_error = connect("clicked", get_node(Utils.START_PATH), "_on_LevelButton_clicked")
+	var _self_conn_error = connect("pressed", self, "_on_self_pressed")
+	var _menu_conn_error = connect("level_pressed", get_node(Utils.START_PATH), "_on_LevelButton_pressed")
 
 
-func _on_self_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		var mouse_event = event as InputEventMouseButton
-		if mouse_event.is_pressed() and mouse_event.button_index == 1:
-			emit_signal("clicked", text)
+func _on_self_pressed() -> void:
+	emit_signal("level_pressed", text)
--- scenes/menus/start_menu/start_menu.gd
+++ scenes/menus/start_menu/start_menu.gd
@@ -9,50 +9,70 @@ const LEVELS_MAP = {
 }
 
 onready var version_label := $Version as Label
+
+onready var start_button := $TitleContainer/VBoxContainer/CenterContainer2/VBoxContainer/StartButton as Button
+onready var controls_button := $TitleContainer/VBoxContainer/CenterContainer2/VBoxContainer/ControlsButton as Button
+onready var first_level_button := $LevelsContainer/Panel/MarginContainer/VBoxContainer/CenterContainer2/VBoxContainer/SlimesLevel as Button
+onready var exit_controls_button := $ControlsContainer/Panel/MarginContainer/ExitControlsButton as TextureButton
+
 onready var controls_container := $ControlsContainer as MarginContainer
 onready var levels_container := $LevelsContainer as MarginContainer
 
 onready var main := preload("res://scenes/main/main.tscn").instance() as Main
 
+var menu_open := false
+
 
 func _ready() -> void:
+	start_button.grab_focus()
 	get_tree().paused = false
 	_load_version()
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('ui_cancel'):
+		if menu_open:
+			menu_open = false
+			levels_container.visible = false
+			controls_container.visible = false
+			start_button.grab_focus()
+
+
 func _load_version() -> void:
-	var version_file = File.new()
+	var version_file := File.new()
 	version_file.open(Utils.VERSION_PATH, File.READ)
-	var version_number = version_file.get_as_text()
+	var version_number := version_file.get_as_text()
 	version_label.set_text(version_number)
 
 
-func _on_StartButton_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		var mouse_event = event as InputEventMouseButton
-		if mouse_event.is_pressed() and mouse_event.button_index == 1:
-			levels_container.visible = true
-
-
-func _on_ControlsButton_gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		var mouse_event = event as InputEventMouseButton
-		if mouse_event.is_pressed() and mouse_event.button_index == 1:
-			controls_container.visible = true
-
-
 func _on_ExitControlsButton_pressed() -> void:
+	menu_open = false
 	controls_container.visible = false
+	controls_button.grab_focus()
 
 
 func _on_ExitLevelsButton_pressed() -> void:
+	menu_open = false
 	levels_container.visible = false
+	start_button.grab_focus()
 
 
-func _on_LevelButton_clicked(level: String) -> void:
-	var level_file = LEVELS_MAP[level] as String
+func _on_LevelButton_pressed(level: String) -> void:
+	var level_file := LEVELS_MAP[level] as String
 	var level_path := Utils.LEVELS_DIR_PATH + ("%s/%s.tscn" % [level_file, level_file]) as String
 	main.load_level(level_path)
 	get_tree().get_root().add_child(main)
 	get_tree().set_current_scene(main)
 	queue_free()
+
+
+func _on_ControlsButton_pressed() -> void:
+	menu_open = true
+	controls_container.visible = true
+	exit_controls_button.grab_focus()
+
+
+func _on_StartButton_pressed() -> void:
+	menu_open = true
+	levels_container.visible = true
+	first_level_button.grab_focus()
