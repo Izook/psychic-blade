d8ed9a49a7b355b311f4783452f8e60bf01d2649
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-02-09 20:17:53 -0800

    Add UI sound effects to start menu

--- scenes/menus/start_menu/start_menu.gd
+++ scenes/menus/start_menu/start_menu.gd
@@ -18,26 +18,37 @@ onready var exit_controls_button := $ControlsContainer/Panel/MarginContainer/Exi
 onready var controls_container := $ControlsContainer as MarginContainer
 onready var levels_container := $LevelsContainer as MarginContainer
 
-onready var music_player := $MusicPlayer as AudioStreamPlayer
+onready var music_player := $Audio/MusicPlayer as AudioStreamPlayer
+onready var select_sound_player := $Audio/SelectSoundPlayer as AudioStreamPlayer
+onready var confirm_sound_player := $Audio/ConfirmSoundPlayer as AudioStreamPlayer
+onready var cancel_sound_player := $Audio/CancelSoundPlayer as AudioStreamPlayer
+onready var quit_sound_player := $Audio/QuitSoundPlayer as AudioStreamPlayer
 
 onready var main := preload("res://scenes/main/main.tscn").instance() as Main
 
 var menu_open := false
+var focus_grabbed = false
 
 
 func _ready() -> void:
-	start_button.grab_focus()
 	get_tree().paused = false
 	_load_version()
 
 
 func _input(event: InputEvent) -> void:
+	
+	if event.is_action_pressed("ui_up") or event.is_action_pressed("ui_down")  or event.is_action_pressed("ui_left")  or event.is_action_pressed("ui_right"):
+		if not focus_grabbed:
+			focus_grabbed = true
+			start_button.grab_focus()
+	
 	if event.is_action_pressed('ui_cancel'):
 		if menu_open:
 			menu_open = false
 			levels_container.visible = false
 			controls_container.visible = false
 			start_button.grab_focus()
+			cancel_sound_player.play()
 
 
 func _load_version() -> void:
@@ -51,18 +62,26 @@ func _on_ExitControlsButton_pressed() -> void:
 	menu_open = false
 	controls_container.visible = false
 	controls_button.grab_focus()
+	cancel_sound_player.play()
 
 
 func _on_ExitLevelsButton_pressed() -> void:
 	menu_open = false
 	levels_container.visible = false
 	start_button.grab_focus()
+	cancel_sound_player.play()
+	select_sound_player.set_volume_db(-80.0)
+	yield(cancel_sound_player, "finished")
+	select_sound_player.set_volume_db(0.0)
 
 
 func _on_LevelButton_pressed(level: String) -> void:
+	get_tree().get_root().set_disable_input(true)
 	var level_file := LEVELS_MAP[level] as String
 	var level_path := Utils.LEVELS_DIR_PATH + ("%s/%s.tscn" % [level_file, level_file]) as String
 	main.load_level(level_path)
+	confirm_sound_player.play()
+	yield(confirm_sound_player, "finished")
 	get_tree().get_root().add_child(main)
 	get_tree().set_current_scene(main)
 	queue_free()
@@ -72,17 +91,25 @@ func _on_ControlsButton_pressed() -> void:
 	menu_open = true
 	controls_container.visible = true
 	exit_controls_button.grab_focus()
+	confirm_sound_player.play()
 
 
 func _on_StartButton_pressed() -> void:
 	menu_open = true
 	levels_container.visible = true
 	first_level_button.grab_focus()
+	confirm_sound_player.play()
 
 
 func _on_QuitPlayingButton_pressed() -> void:
+	quit_sound_player.play()
+	yield(quit_sound_player, "finished")
 	get_tree().quit()
 
 
 func _on_MusicPlayer_finished() -> void:
 	music_player.play()
+
+
+func _on_Button_focus_entered() -> void:
+	select_sound_player.play()
