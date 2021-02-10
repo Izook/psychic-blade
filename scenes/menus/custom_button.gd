3d12b065b52be7b949ec052a7e4cda8ff158cb4e
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-02-09 22:02:57 -0800

    Improve mouse experience with custom button script in start menu

---
+++ scenes/menus/custom_button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+class_name CustomButton
+
+
+func _ready() -> void:
+	var _enter_conn_error := connect("mouse_entered", self, "_on_self_mouse_entered")
+	var _exit_conn_error := connect("mouse_exited", self, "_on_self_mouse_entered")
+
+
+func _on_self_mouse_entered() -> void:
+	grab_focus()
+
+
+func _on_self_mouse_exited() -> void:
+	release_focus()
