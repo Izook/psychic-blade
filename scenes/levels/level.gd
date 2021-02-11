79711bb6139e3bf5ba25a879b98b327c3f7f4f40
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-02-10 19:59:35 -0800

    Write music player class for easier improvements

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,13 +1,3 @@
 extends Node
 
 class_name Level
-
-onready var music_player := $MusicPlayer as AudioStreamPlayer
-
-
-func _ready() -> void:
-	var _conn_error := music_player.connect("finished", self, "_on_MusicPlayer_finished")
-
-
-func _on_MusicPlayer_finished() -> void:
-	music_player.play()
