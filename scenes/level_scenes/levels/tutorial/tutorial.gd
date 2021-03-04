329a116c277b332ed12e7c1b3ed3d7c4693ab9fd
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-03-03 22:46:27 -0800

    Finish radius changing room

--- scenes/level_scenes/levels/tutorial/tutorial.gd
+++ scenes/level_scenes/levels/tutorial/tutorial.gd
@@ -12,13 +12,18 @@ onready var blade_spinning_braziers := [
 ] as Array
 onready var blade_spinning_door := $BladeSpinningRoom/Doors as TileMap
 
-var cleared_blade_spinning_room := false
+onready var radius_changing_brazier := $RadiusChangingRoom/Braziers/Brazier1 as Brazier
+onready var radius_changing_door := $RadiusChangingRoom/Doors as TileMap
 
+var cleared_blade_spinning_room := false
+var cleared_radius_changing_room := false
 
 func _ready() -> void:
 	
 	for brazier in blade_spinning_braziers:
 		brazier.connect("put_out", self, "_on_BladeSpinningBrazier_put_out")
+		
+	radius_changing_brazier.connect("put_out", self, "_on_RadiusChangingBrazier_put_out")
 
 
 func _on_BladeSpinningBrazier_put_out() -> void:
@@ -36,3 +41,14 @@ func _on_BladeSpinningBrazier_put_out() -> void:
 			blade_spinning_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
 			blade_spinning_door.set_collision_mask_bit(Utils.BLADE_COLLISION_LAYER, false)
 			cleared_blade_spinning_room = true
+
+
+func _on_RadiusChangingBrazier_put_out() -> void:
+	if not cleared_radius_changing_room:
+			success_sound_player.play()
+			radius_changing_door.visible = false
+			radius_changing_door.set_collision_layer_bit(Utils.ENTITIES_COLLISION_LAYER, false)
+			radius_changing_door.set_collision_mask_bit(Utils.ENTITIES_COLLISION_LAYER, false)
+			radius_changing_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
+			radius_changing_door.set_collision_mask_bit(Utils.BLADE_COLLISION_LAYER, false)
+			cleared_radius_changing_room = true
