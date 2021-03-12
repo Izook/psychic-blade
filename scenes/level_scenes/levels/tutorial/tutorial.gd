9902fae460e8352415994baabfca5b6feee32009
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-03-11 21:47:09 -0800

    Add zooming and blade throwing room

--- scenes/level_scenes/levels/tutorial/tutorial.gd
+++ scenes/level_scenes/levels/tutorial/tutorial.gd
@@ -15,8 +15,12 @@ onready var blade_spinning_door := $BladeSpinningRoom/Doors as TileMap
 onready var radius_changing_brazier := $RadiusChangingRoom/Braziers/Brazier1 as Brazier
 onready var radius_changing_door := $RadiusChangingRoom/Doors as TileMap
 
+onready var blade_throwing_brazier := $BladeThrowingRoom/Braziers/Brazier1 as Brazier
+onready var blade_throwing_door := $BladeThrowingRoom/Doors as TileMap
+
 var cleared_blade_spinning_room := false
 var cleared_radius_changing_room := false
+var cleared_blade_throwing_room := false
 
 func _ready() -> void:
 	
@@ -24,6 +28,7 @@ func _ready() -> void:
 		brazier.connect("put_out", self, "_on_BladeSpinningBrazier_put_out")
 		
 	radius_changing_brazier.connect("put_out", self, "_on_RadiusChangingBrazier_put_out")
+	blade_throwing_brazier.connect("put_out", self, "_on_BladeThrowingBrazier_put_out")
 
 
 func _on_BladeSpinningBrazier_put_out() -> void:
@@ -52,3 +57,14 @@ func _on_RadiusChangingBrazier_put_out() -> void:
 			radius_changing_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
 			radius_changing_door.set_collision_mask_bit(Utils.BLADE_COLLISION_LAYER, false)
 			cleared_radius_changing_room = true
+
+
+func _on_BladeThrowingBrazier_put_out() -> void:
+	if not cleared_blade_throwing_room:
+		success_sound_player.play()
+		blade_throwing_door.visible = false
+		blade_throwing_door.set_collision_layer_bit(Utils.ENTITIES_COLLISION_LAYER, false)
+		blade_throwing_door.set_collision_mask_bit(Utils.ENTITIES_COLLISION_LAYER, false)
+		blade_throwing_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
+		blade_throwing_door.set_collision_mask_bit(Utils.BLADE_COLLISION_LAYER, false)
+		cleared_blade_throwing_room = true
