af415bd8ea2ac05b82bef73abb00a89274d079c8
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-03-12 20:55:30 -0800

    Decorate all of the tutorial so far

--- scenes/level_scenes/levels/tutorial/tutorial.gd
+++ scenes/level_scenes/levels/tutorial/tutorial.gd
@@ -11,12 +11,15 @@ onready var blade_spinning_braziers := [
 	$BladeSpinningRoom/Braziers/Brazier4
 ] as Array
 onready var blade_spinning_door := $BladeSpinningRoom/Doors as TileMap
+onready var blade_spinning_door_sprites := $BladeSpinningRoom/DoorsSprites as TileMap
 
 onready var radius_changing_brazier := $RadiusChangingRoom/Braziers/Brazier1 as Brazier
 onready var radius_changing_door := $RadiusChangingRoom/Doors as TileMap
+onready var radius_chaning_door_sprites := $RadiusChangingRoom/DoorsSprites as TileMap
 
 onready var blade_throwing_brazier := $BladeThrowingRoom/Braziers/Brazier1 as Brazier
 onready var blade_throwing_door := $BladeThrowingRoom/Doors as TileMap
+onready var blade_throwing_sprites := $BladeThrowingRoom/DoorSprites as TileMap
 
 var cleared_blade_spinning_room := false
 var cleared_radius_changing_room := false
@@ -41,6 +44,7 @@ func _on_BladeSpinningBrazier_put_out() -> void:
 		if not cleared_blade_spinning_room:
 			success_sound_player.play()
 			blade_spinning_door.visible = false
+			blade_spinning_door_sprites.visible = false
 			blade_spinning_door.set_collision_layer_bit(Utils.ENTITIES_COLLISION_LAYER, false)
 			blade_spinning_door.set_collision_mask_bit(Utils.ENTITIES_COLLISION_LAYER, false)
 			blade_spinning_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
@@ -52,6 +56,7 @@ func _on_RadiusChangingBrazier_put_out() -> void:
 	if not cleared_radius_changing_room:
 			success_sound_player.play()
 			radius_changing_door.visible = false
+			radius_chaning_door_sprites.visible = false
 			radius_changing_door.set_collision_layer_bit(Utils.ENTITIES_COLLISION_LAYER, false)
 			radius_changing_door.set_collision_mask_bit(Utils.ENTITIES_COLLISION_LAYER, false)
 			radius_changing_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
@@ -63,6 +68,7 @@ func _on_BladeThrowingBrazier_put_out() -> void:
 	if not cleared_blade_throwing_room:
 		success_sound_player.play()
 		blade_throwing_door.visible = false
+		blade_throwing_sprites.visible = false
 		blade_throwing_door.set_collision_layer_bit(Utils.ENTITIES_COLLISION_LAYER, false)
 		blade_throwing_door.set_collision_mask_bit(Utils.ENTITIES_COLLISION_LAYER, false)
 		blade_throwing_door.set_collision_layer_bit(Utils.BLADE_COLLISION_LAYER, false)
