e0fa382cb1ce9490cfeb3f59b464d94e7f204e7c
Isaac Ortega <isaac.io.ortega@gmail.com>
2021-03-12 22:48:40 -0800

    Have braziers finish being put out before emitting put out signal for game feel

--- scenes/interactables/brazier/brazier.gd
+++ scenes/interactables/brazier/brazier.gd
@@ -28,23 +28,25 @@ func set_state(new_state: int) -> void:
 	
 	match new_state:
 		BrazierState.FLAME_OUT:
-			emit_signal("put_out")
 			flame_particles.emitting = false
 			flame_reset_timer.start(flame_reset_time)
 			
 			if brazier_state == BrazierState.FLAME_LIT:
 				sparks_particles.emitting = true
 				put_out_audio_player.play()
+				brazier_state = new_state
+				yield(put_out_audio_player, "finished")
+				emit_signal("put_out")
 			
 		BrazierState.FLAME_LIT:
-			emit_signal("relit")
 			flame_particles.emitting = true
 			
 			if brazier_state == BrazierState.FLAME_OUT:
 				sparks_particles.emitting = true
 				relit_audio_player.play()
-	
-	brazier_state = new_state
+				brazier_state = new_state
+				yield(relit_audio_player, "finished")
+				emit_signal("relit")
 
 
 func _on_Brazier_body_entered(body: Node) -> void:
